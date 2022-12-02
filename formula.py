98b7dd962e74d9cff0ebb71ba74b9fc11ca3cf4a
Juan  D~ <99096029+JuanDiaz00@users.noreply.github.com>
2022-12-01 21:05:51 -0500

    Creación de la formula
    Después se edita

---
+++ formula.py
@@ -0,0 +1,17 @@
+# Importamos la biblioteca necesaria para trabajar con números y operaciones matemáticas
+import math
+
+# Definimos la función para calcular el porcentaje de efectividad del cultivo
+def calculate_cultivation_effectiveness(terrain, conditions):
+  # Definimos una constante que representa el nivel mínimo de humedad del suelo necesario para que las fresas crezcan bien
+  MIN_SOIL_MOISTURE = 70
+
+  # Definimos una constante que representa el nivel máximo de plagas que pueden tolerarse en el cultivo
+  MAX_PEST_LEVEL = 30
+
+  # Definimos una constante que representa el factor de influencia de cada condición en el resultado del cultivo
+  TERRAIN_FACTOR = 0.3
+  CONDITIONS_FACTOR = 0.7
+
+  # Calculamos el resultado del cultivo en función del terreno y las condiciones
+  result = TERRAIN_FACTOR * terrain + CONDITIONS_F
